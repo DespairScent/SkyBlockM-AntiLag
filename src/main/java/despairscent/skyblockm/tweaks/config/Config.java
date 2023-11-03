8b65d0420f8e221746e9a5cb963a56054372bf7e
DespairScent <74109497+DespairScent@users.noreply.github.com>
2023-11-03 10:36:19 +0200

    Новое: исправление видимых рамок у хранилища

--- src/main/java/despairscent/skyblockm/tweaks/config/Config.java
+++ src/main/java/despairscent/skyblockm/tweaks/config/Config.java
@@ -31,6 +31,7 @@ public class Config {
 
     public static class QoL {
         public boolean storageTargetingFix = true;
+        public boolean storageFramesFix = true;
         public boolean moreTooltipInfo = true;
     }
 
